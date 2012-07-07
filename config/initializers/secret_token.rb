74869e72969af053360b6107a04710bb443dffbd
Christian Berkhoff <christian.berkhoff@gmail.com>
2012-07-07 12:44:07 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hunchator::Application.config.secret_token = '91fc3809d42ab66e259aca42c017af962c30ee5adf5303458cf2051e1aa266adbf6cd95519409f23ca1fa9111d02c15e13d10e73c79d9e85785ab7f64e7ada30'
